1956b0074be0fa5d4fe5b67f39bb0e214d905251
Bohdan Robota <newbusines123@gmail.com>
2023-07-03 17:55:48 +0300

    feat: Implement test task

---
+++ src/components/PasswordTest.tsx
@@ -0,0 +1,104 @@
+import React, { useEffect, useState } from 'react';
+import './PasswordTest.css';
+import { Input, Container, Box, Text } from '@chakra-ui/react';
+import { complexityEnum, colorsEnum } from './enum/passwordTest.enum';
+
+function PasswordTest() {
+  const [password, setPassword] = useState('');
+  const [passComplexity, setPassComplexity] = useState(complexityEnum.EMPTY);
+  const [easyFieldColor, setEasyFieldColor] = useState(colorsEnum.GREY);
+  const [mediumFieldColor, setMediumFieldColor] = useState(colorsEnum.GREY);
+  const [strongFieldColor, setStrongFieldColor] = useState(colorsEnum.GREY);
+
+  function passwordHandler(event: React.ChangeEvent<HTMLInputElement>) {
+    setPassword(event.target.value);
+    currentComplexity(event.target.value);
+  }
+
+  function currentComplexity(password: string) {
+    checkIsEmpty(password);
+    checkEasy(password);
+    checkMedium(password);
+    checkStrong(password);
+  }
+
+  function checkIsEmpty(password: string) {
+    if (password.length === 0) {
+      setPassComplexity(complexityEnum.EMPTY);
+    } else {
+      setPassComplexity(complexityEnum.INVALID);
+    }
+  }
+
+  function checkStrong(password: string) {
+    const result = password.match(/^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[^\w]).{8,}/);
+    if (result) setPassComplexity(complexityEnum.STRONG);
+  }
+
+  function checkEasy(password: string) {
+    const result = password.match(/^.{8,}/);
+    if (result) setPassComplexity(complexityEnum.EASY);
+  }
+
+  function checkMedium(password: string) {
+    const result =
+      password.match(/^(?=.*[A-Za-z])(?=.*[0-9]).{8,}/) ||
+      password.match(/^(?=.*[A-Za-z])(?=.*[^\w]).{8,}/) ||
+      password.match(/^(?=.*[0-9])(?=.*[^\w]).{8,}/);
+    if (result) setPassComplexity(complexityEnum.MEDIUM);
+  }
+
+  useEffect(() => {
+    setColorField();
+  }, [passComplexity]);
+
+  function setColorField() {
+    switch (passComplexity) {
+      case complexityEnum.INVALID:
+        setEasyFieldColor(colorsEnum.RED);
+        setMediumFieldColor(colorsEnum.RED);
+        setStrongFieldColor(colorsEnum.RED);
+        return null;
+      case complexityEnum.EASY:
+        setEasyFieldColor(colorsEnum.RED);
+        setMediumFieldColor(colorsEnum.GREY);
+        setStrongFieldColor(colorsEnum.GREY);
+        return null;
+      case complexityEnum.MEDIUM:
+        setEasyFieldColor(colorsEnum.YELLOW);
+        setMediumFieldColor(colorsEnum.YELLOW);
+        setStrongFieldColor(colorsEnum.GREY);
+        return null;
+      case complexityEnum.STRONG:
+        setEasyFieldColor(colorsEnum.GREEN);
+        setMediumFieldColor(colorsEnum.GREEN);
+        setStrongFieldColor(colorsEnum.GREEN);
+        return null;
+      default:
+        setEasyFieldColor(colorsEnum.GREY);
+        setMediumFieldColor(colorsEnum.GREY);
+        setStrongFieldColor(colorsEnum.GREY);
+        return null;
+    }
+  }
+
+  return (
+    <Container className="container">
+      <Input value={password} onChange={passwordHandler} type="password" placeholder="password" className="inputBox" />
+      <Text className="field" color={'grey'}>
+        {'Password is ' + passComplexity}
+      </Text>
+      <Box className="field" bg={easyFieldColor}>
+        Easy Password
+      </Box>
+      <Box className="field" bg={mediumFieldColor}>
+        Medium password
+      </Box>
+      <Box className="field" bg={strongFieldColor}>
+        Strong password
+      </Box>
+    </Container>
+  );
+}
+
+export default PasswordTest;
---
+++ src/components/enum/passwordTest.enum.ts
@@ -0,0 +1,13 @@
+export enum complexityEnum {
+  EMPTY = 'empty',
+  INVALID = 'invalid',
+  EASY = 'easy',
+  MEDIUM = 'medium',
+  STRONG = 'strong',
+}
+export enum colorsEnum {
+  GREY = 'grey',
+  RED = 'tomato',
+  YELLOW = '#FDDA0D',
+  GREEN = '#50C878',
+}
